696eda3d569668dc5bf8e2d567016ee2e6ff4450
ekksttasy <114907880+ekksttasy@users.noreply.github.com>
2025-04-02 14:48:35 +0100

    first PROPER commit

---
+++ Assets/LogicScript.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class LogicScript : MonoBehaviour
+{
+    public int playerScore;
+    public Text scoreText;
+    public GameObject gameOverScreen;
+
+    void Start()
+    {
+        gameOverScreen.SetActive(false);
+    }
+
+    [ContextMenu("Increase Score")]
+    public void addScore(int scoreToAdd)
+    {
+        playerScore += scoreToAdd;
+        scoreText.text = playerScore.ToString();
+    }
+
+    public void restartGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        gameOverScreen.SetActive(false);
+    }
+
+    public void gameOver()
+    {
+        gameOverScreen.SetActive(true);
+    }
+
+}
---
+++ Assets/MainMenuScript.cs
@@ -0,0 +1,30 @@
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuScript : MonoBehaviour
+{
+    public void GameStart() => SceneManager.LoadScene("SinglePlayer");
+    public void MultiplayerStart() => SceneManager.LoadScene("ServerList");
+
+    public void QuitGame()
+    {
+        Application.Quit();
+
+        #if UNITY_EDITOR
+        EditorApplication.ExitPlaymode();
+        #endif
+    }
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
