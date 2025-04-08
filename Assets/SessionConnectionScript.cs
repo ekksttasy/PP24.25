9036d09f9105e9e0248a1ddd6453fadbf40e9f51
ekksttasy <114907880+ekksttasy@users.noreply.github.com>
2025-04-08 19:26:06 +0100

    SessionScript allows for lobby hosting/creation. + playerBird variant

---
+++ Assets/SessionConnectionScript.cs
@@ -0,0 +1,108 @@
+using UnityEngine;
+using Unity.Services.Multiplayer;
+using System.Data;
+using Unity.Netcode;
+using Unity.Services.Core;
+using System.Threading.Tasks;
+using Unity.Services.Authentication;
+using System;
+
+public class SessionConnectionScript : MonoBehaviour
+{
+    private string playerName;
+    public int playerCount;
+    private int maxPlayers = 4;
+    private string lobbyName;
+
+    private ConnectionState state = ConnectionState.Disconnected;
+    private ISession session;
+    private NetworkManager NetworkManager;
+
+    private enum ConnectionState
+    {
+        Disconnected,
+        Connecting,
+        Connected
+    }
+
+    private async void Awake()
+    {
+        NetworkManager = GetComponent<NetworkManager>();
+        NetworkManager.OnClientConnectedCallback += OnClientConnection;
+        NetworkManager.OnSessionOwnerPromoted += OnPartyLeaderPromotion;
+        await UnityServices.InitializeAsync();
+    }
+
+    private void OnGUI()
+    {
+        if (state == ConnectionState.Connected)
+            return;
+
+        GUI.enabled = state != ConnectionState.Connecting;
+        using (new GUILayout.HorizontalScope(GUILayout.Width(250)))
+        {
+            GUILayout.Label("Profile Name", GUILayout.Width(100));
+            playerName = GUILayout.TextField(playerName);
+        }
+
+        using (new GUILayout.HorizontalScope(GUILayout.Width(250)))
+        {
+            GUILayout.Label("Session Name", GUILayout.Width(100));
+            lobbyName = GUILayout.TextField(lobbyName);
+        }
+
+        GUI.enabled = GUI.enabled && !string.IsNullOrEmpty(playerName) && !string.IsNullOrEmpty(lobbyName);
+
+        if (GUILayout.Button("Host / Join a Lobby"))
+        {
+            HostorJoinSession();
+        }
+    }
+
+    private void OnClientConnection(ulong clientId)
+    {
+        if (NetworkManager.LocalClientId == clientId)
+        {
+            Debug.Log($"Client-{clientId} is connected");
+            
+        }
+    }
+
+    private void OnPartyLeaderPromotion(ulong sessionOwnerChange)
+    {
+        if (NetworkManager.LocalClient.IsSessionOwner)
+        {
+            Debug.Log($"Client-{NetworkManager.LocalClientId} is the party leader");
+        }
+    }
+
+    private async void HostorJoinSession()
+    {
+        state = ConnectionState.Connecting;
+
+        try
+        {
+            AuthenticationService.Instance.SwitchProfile(playerName);
+            await AuthenticationService.Instance.SignInAnonymouslyAsync();
+
+            var options = new SessionOptions()
+            {
+                Name = lobbyName,
+                MaxPlayers = maxPlayers
+            }.WithDistributedAuthorityNetwork();
+
+            session = await MultiplayerService.Instance.CreateOrJoinSessionAsync(lobbyName, options);
+            state = ConnectionState.Connected;
+        }
+        catch (Exception e)
+        {
+            state = ConnectionState.Disconnected;
+            Debug.LogException(e);
+        }
+    }
+
+    private void OnDestroy()
+    {
+        session?.LeaveAsync();
+    }
+}
